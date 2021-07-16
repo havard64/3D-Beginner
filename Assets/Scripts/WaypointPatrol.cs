7d7f9c29b1ad2e83e48b9663982ba9e35ff157ac
havard64 <hhel@online.no>
2021-07-16 09:47:06 +0200

    Ghost seeing player
    Start with states

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -9,6 +9,11 @@ public class WaypointPatrol : MonoBehaviour
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
     int m_CurrentWaypointIndex;
+    public Transform player;
+
+    // Field of view setttings
+    float fovDist = 20.0f;
+    float focAngle = 45.0f;
 
     void Start()
     {
@@ -16,8 +21,25 @@ public class WaypointPatrol : MonoBehaviour
     }
 
 
+    bool ICanSee(Transform player)
+    {
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, direction, out hit) && hit.collider.gameObject.tag == "Player" && direction.magnitude < fovDist && angle < focAngle)
+        {
+            Debug.Log("Can see player at position: " + player.position);
+            return true;
+        }
+        Debug.Log("I can't see the player" + this.transform.position + "player position: " + player.position + "direction: " + direction + "angle: " + angle + "magnitude: " + direction.magnitude + "hit: " + Physics.Raycast(this.transform.position, direction, out hit) + "tag: " + hit.collider.gameObject.tag);
+        return false;
+    }
+
+
     void Update()
     {
+        ICanSee(player);
         if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
