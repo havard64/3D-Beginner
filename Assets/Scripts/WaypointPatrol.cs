b2dd2c4781212185b31ad0aab0401f4956bd193e
havard64 <hhel@online.no>
2021-07-19 10:15:38 +0200

    Fully implemented game
    Implemented chasing, patroling and investigating

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -17,11 +17,19 @@ public class WaypointPatrol : MonoBehaviour
 
     // FSM
     enum State { Patrol, Investigate, Chase };
-    State CurState = State.Patrol;
+    State curState = State.Patrol;
+
+    // Patrol settings
+    float patrolWait = 2.0f;
+    float patrolTimePassed = 0;
+
+    // Last place the player was seen
+    Vector3 lastPlaceSeen;
 
     void Start()
     {
         navMeshAgent.SetDestination(waypoints[0].position);
+        lastPlaceSeen = this.transform.position;
     }
 
 
@@ -33,31 +41,76 @@ public class WaypointPatrol : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(this.transform.position, direction, out hit) && hit.collider.gameObject.tag == "Player" && direction.magnitude < fovDist && angle < focAngle)
         {
-            Debug.Log("Can see player at position: " + player.position);
             return true;
         }
-        Debug.Log("I can't see the player" + this.transform.position + "player position: " + player.position + "direction: " + direction + "angle: " + angle + "magnitude: " + direction.magnitude + "hit: " + Physics.Raycast(this.transform.position, direction, out hit) + "tag: " + hit.collider.gameObject.tag);
         return false;
     }
 
-
-    void Update()
+    void Patrol()
     {
-                
-        if (ICanSee(player))
-        {
-            CurState = State.Chase;
-            navMeshAgent.ResetPath();
-            navMeshAgent.SetDestination(player.position);
-        }
-        else
-        {
-            if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
             {
                 m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
                 navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
             }
-            
+    }
+
+    void Investigate()
+    {
+        // if the agent has arrived at the investigating goal, the agent should start to patrol
+        if (Vector3.Distance(this.transform.position, lastPlaceSeen) < 0.04)
+        {
+            patrolTimePassed += Time.deltaTime;
+            if (patrolTimePassed > patrolWait)
+            {
+                navMeshAgent.ResetPath();
+                navMeshAgent.SetDestination(waypoints[0].position);
+                curState = State.Patrol;
+                patrolTimePassed = 0;
+            }
+        }
+        else
+        {
+            navMeshAgent.SetDestination(lastPlaceSeen);
+        }
+    }
+
+    void Chase()
+    {
+        navMeshAgent.ResetPath();
+        navMeshAgent.SetDestination(player.position);
+    }
+
+    void Update()
+    {
+        State tmpState = curState; // Check if the state has changed
+
+
+        if (ICanSee(player))
+        {
+            curState = State.Chase;
+            lastPlaceSeen = player.position;
+        }
+        else
+        {
+            if (curState == State.Chase)
+            {
+                curState = State.Investigate;
+            }
+        }  
+
+        // State check
+        switch(curState)
+        {
+            case State.Patrol: // Start patrolling
+                Patrol();
+                break;
+            case State.Investigate: // Start investigate
+                Investigate();
+                break;
+            case State.Chase: // Start chasing
+                Chase();
+                break;
         }
     }
 }
